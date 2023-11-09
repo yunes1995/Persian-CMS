ff28c0127abb873e030e3ab0d55aade3d6c243ca
yunes moahammadi <younesm837@gmail.com>
2023-11-09 12:12:47 +0330

    some bugs in header has been fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import Sidebar from "./component/sidebar/Sidebar";
-import "./Custom.css"
+import "./Custom.css";
+import Header from './component/Header/Header';
+import "./App.css";
 
 export default function App() {
   return (
-   <Sidebar></Sidebar>
-  )
+    <>
+      <Sidebar />
+
+      <div className="main">
+        <Header />
+        {/* Router */}
+      </div>
+    </>
+  );
 }
